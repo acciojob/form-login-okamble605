50bd1cd7cd2f2ed7cee06526bc886d0783e7e2d0
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2024-12-19 18:44:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent the form from submitting
+    event.preventDefault();
 
+    // Get the form element
+    const form = document.getElementById("form1");
+
+    // Get the values of the first and last name inputs
+    const firstName = form.elements["fname"].value;
+    const lastName = form.elements["lname"].value;
+
+    // Display the values using an alert
+    alert(`${firstName} ${lastName}`);
 }
